f08b4290c9875adb61f181a1bfde1c4f94e36c43
kcavagnolo <ken_cavagnolo@yahoo.com>
2015-06-25 18:17:22 -0400

    Changes

--- oauth.py
+++ oauth.py
@@ -1,61 +1,53 @@
 #!/usr/bin/env python
 
+import os
 import oauth2 as oauth
 import urlparse
+import hashlib
+import webbrowser
+import urllib
+import urllib2
+import ast
 
-consumer_key = "scrape_deez_nutz"
-consumer_secret = "scrape_deez_nutz"
+consumer_key = str(os.environ['LINKEDIN_API_KEY'])
+consumer_secret = str(os.environ['LINKEDIN_API_SECRET'])
+redirect_uri = 'http%3A%2F%2Flocalhost%3A8080%2Fcode'
 
-class LinkedIn(object):
-    
-    def __init__(self, consumer_key, consumer_secret):
-        self.consumer_key = consumer_key
-        self.consumer_secret = consumer_secret
-        
-    def request_token(self):
-        self.consumer = oauth.Consumer(consumer_key, consumer_secret)
-        client = oauth.Client(self.consumer)
-        request_token_url      = 'https://api.linkedin.com/uas/oauth/requestToken?scope=r_network'
-        resp, content = client.request(request_token_url, "POST")
-        if resp['status'] != '200':
-            raise Exception("Invalid response %s." % resp['status'])
-        self.request_token = dict(urlparse.parse_qsl(content))
-        print "Request Token:"
-        print "    - oauth_token        = %s" % self.request_token['oauth_token']
-        print "    - oauth_token_secret = %s" % self.request_token['oauth_token_secret']
-        print
-        
-    def authorize(self):
-        authorize_url =      'https://api.linkedin.com/uas/oauth/authorize'
-        print "Go to the following link in your browser:"
-        print "%s?oauth_token=%s" % (authorize_url, self.request_token['oauth_token'])
-        print
-        accepted = 'n'
-        while accepted.lower() == 'n':
-            accepted = raw_input('Have you authorized me? (y/n) ')
-        self.oauth_verifier = raw_input('What is the PIN? ')
-        
-    def access(self):
-        access_token_url = 'https://api.linkedin.com/uas/oauth/accessToken'
-        token = oauth.Token(self.request_token['oauth_token'], self.request_token['oauth_token_secret'])
-        token.set_verifier(self.oauth_verifier)
-        client = oauth.Client(self.consumer, token)
-        resp, content = client.request(access_token_url, "POST")
-        self.access_token = dict(urlparse.parse_qsl(content))
-        print "Access Token:"
-        print "    - oauth_token        = %s" % self.access_token['oauth_token']
-        print "    - oauth_token_secret = %s" % self.access_token['oauth_token_secret']
-        print
-        print "You may now access protected resources using the access tokens above."
-        print 
-        
-    def dance(self):
-        self.request_token()
-        self.authorize()
-        self.access()
+random_data = os.urandom(128)
+state_key = hashlib.md5(random_data).hexdigest()[:16]
 
+request_token_url = 'https://www.linkedin.com/uas/oauth2/authorization?'
+access_token_url = 'https://www.linkedin.com/uas/oauth2/accessToken?'
 
+consumer = oauth.Consumer(consumer_key, consumer_secret)
+client = oauth.Client(consumer)
+params = '&'.join(
+    ["response_type=code",
+     "client_id="+consumer_key,
+     "redirect_uri="+redirect_uri,
+     "state="+state_key,
+     "scope=r_basicprofile"])
+resp, content = client.request(request_token_url+params, "GET")
+if resp['status'] != '200':
+    raise Exception("Invalid response %s." % resp['status'])
+authorize_url = resp['content-location']
 
-if __name__ == '__main__':
-    l = LinkedIn(consumer_key, consumer_secret)
-    l.dance()
+webbrowser.open(authorize_url,new=2)
+key = raw_input('Paste the code here: ')
+state = key.split('&')[1].split('=')[1]
+if state != state_key:
+   raise Exception("State key error. Being attacked?")
+oauth_verifier = key.split('&')[0].split('=')[1]
+
+params = '&'.join(
+    ["grant_type=authorization_code",
+     "code="+oauth_verifier,
+     "redirect_uri="+redirect_uri,
+     "client_id="+consumer_key,
+     "client_secret="+consumer_secret])
+
+resp = urllib2.urlopen(urllib2.Request(access_token_url, params))
+content = resp.read()
+token = ast.literal_eval(content)['access_token']
+print "run >>> setenv LINKEDIN_API_TOKEN \'%s\'" % oauth_verifier
+print "run >>> setenv LINKEDIN_API_TOKEN_SECRET \'%s\'" % token
--- query.py
+++ query.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python
 
+import os
 import oauth2 as oauth
 import urlparse
 import simplejson
 import codecs
+import requests
 
-consumer_key = "scrape_deez_nutz"
-consumer_secret = "scrape_deez_nutz"
-
-OAUTH_TOKEN = "scrape_deez_nutz"
-OAUTH_TOKEN_SECRET = "scrape_deez_nutz"
-
+consumer_key = str(os.environ['LINKEDIN_API_KEY'])
+consumer_secret = str(os.environ['LINKEDIN_API_SECRET'])
+api_token = str(os.environ['LINKEDIN_API_TOKEN'])
 OUTPUT = "linked.csv"
 
+response = requests.get(
+    'https://api.linkedin.com/v1/people/~?format=json',
+    headers={'Authorization': api_token})
+print response.content
+exit()
+
+
 def linkedin_connections():
+
     # Use your credentials to build the oauth client
-    consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-    token = oauth.Token(key=OAUTH_TOKEN, secret=OAUTH_TOKEN_SECRET)
+    consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
+    token = oauth.Token(key=api_token, secret=api_token_secret)
+    print token
     client = oauth.Client(consumer, token)
+
+
     # Fetch first degree connections
     resp, content = client.request('http://api.linkedin.com/v1/people/~/connections?format=json')
     results = simplejson.loads(content)    
+    print results
+
     # File that will store the results
     output = codecs.open(OUTPUT, 'w', 'utf-8')
+
     # Loop thru the 1st degree connection and see how they connect to each other
     for result in results["values"]:
         con = "%s %s" % (result["firstName"].replace(",", " "), result["lastName"].replace(",", " "))
         print >>output, "%s,%s" % ("Ken Cavagnolo",  con)
+
         # This is the trick, use the search API to get related connections
         u = "https://api.linkedin.com/v1/people/%s:(relation-to-viewer:(related-connections))?format=json" % result["id"]
         resp, content = client.request(u)
